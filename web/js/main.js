ea1011805c50b24774760b8ebdcac79d152311fd
LILIANA <LILIANA@DESKTOP-3GEATL8>
2017-07-03 20:46:33 -0500

    v13

--- web/js/main.js
+++ web/js/main.js
@@ -86,7 +86,19 @@ $(document).ready(function(){
 })(jQuery);
 
 $(document).ready(function () {
+var date = new Date();
 
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    if (month < 10)
+        month = "0" + month;
+    if (day < 10)
+        day = "0" + day;
+
+    var today = year + "-" + month + "-" + day;
+    $("#fechap").attr("value", today);
 });
 
 
